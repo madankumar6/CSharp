5aae91202cbb90ec8b29a832c7be0c0929a5827b
madankumar6 <madankumar6@gmail.com>
2015-10-16 13:17:43 +0530

    Revert "index on master: 0c2ba52 Initial commit"
    This reverts commit ce153ac86a92a182fd6e6ba12d3ae8065af49856.

---
+++ LINQ Samples/Restriction Operators/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataSource;
+
+namespace Restriction_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataSourceFactory factory = new DataSourceFactory();
+            
+        }
+    }
+}
