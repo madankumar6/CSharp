b95d83215dbf4e15cd7d29dfa52d982e40439ac4
madankumar6 <madankumar6@gmail.com>
2015-10-27 20:46:51 +0530

    Added projects to the LINQ solution.

---
+++ LINQ Samples/Custom Sequence Operators/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Custom_Sequence_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ LINQ Samples/Join Operators/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Join_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
