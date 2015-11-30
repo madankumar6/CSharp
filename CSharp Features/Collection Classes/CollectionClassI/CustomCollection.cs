f115e6ac87098b5dfa1c9a9367f1eafbfd26ae27
madankumar6 <madankumar6@gmail.com>
2015-11-30 21:09:22 +0530

    Completed the Collection Samples.

---
+++ CSharp Features/Collection Classes/CollectionClassI/CustomCollection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionClassI
+{
+    public class Tokens : IEnumerable
+    {
+        public string[] elements;
+
+        public Tokens(string source, char[] delimiters)
+        {
+            // Parse the string into tokens:
+            elements = source.Split(delimiters);
+        }
+
+        // IEnumerable Interface Implementation: Declaration of the GetEnumerator() method required by IEnumerable
+
+        public IEnumerator GetEnumerator()
+        {
+            return new TokenEnumerator(this);
+        }
+    }
+
+    public class TokenEnumerator : IEnumerator
+    {
+        private int position = -1;
+        private Tokens token;
+
+        public TokenEnumerator(Tokens token)
+        {
+            this.token = token;
+        }
+
+        // Declare the MoveNext method required by IEnumerator:
+
+        public bool MoveNext()
+        {
+            if (position < token.elements.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            return false;
+        }
+
+        // Declare the Reset method required by IEnumerator:
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        // Declare the Current property required by IEnumerator:
+        public object Current
+        {
+            get
+            {
+                return token.elements[position];
+            }
+        }
+    }
+
+}
---
+++ CSharp Features/Collection Classes/CollectionClassI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionClassI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing Tokens by breaking the string into tokens:");
+
+            Tokens token = new Tokens("This is a well-done program.", new char[] { ' ', '-' });
+
+            foreach (string item in token)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.Read();
+        }
+    }
+}
